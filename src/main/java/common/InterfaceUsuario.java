a8fbffd91f4eff6c272776a58480a8028c0fb8ea
Eduardo Henrique de Freitas <duh.henrike@gmail.com>
2017-04-21 15:29:37 -0300

    retirado metodo nao utilizado

--- src/main/java/common/InterfaceUsuario.java
+++ src/main/java/common/InterfaceUsuario.java
@@ -17,15 +17,6 @@ import java.util.List;
 public interface InterfaceUsuario extends Remote {
 
 	/**
-	 * Recebe a listagem de contatos Online do servidor
-	 * 
-	 * @param lista
-	 *            Lista de contatos que estao disponiveis
-	 * @throws RemoteException
-	 */
-	public void receberContatosOnline(List<EntidadeUsuario> lista) throws RemoteException;
-
-	/**
 	 * Metodo para recebimento de mensagem
 	 * 
 	 * @param remetente
@@ -54,6 +45,6 @@ public interface InterfaceUsuario extends Remote {
 	 *            Lista de usuarios conectados
 	 * @throws RemoteException
 	 */
-	public void receberListaParticipantes(ArrayList<EntidadeUsuario> arrayList) throws RemoteException;
+	public void receberListaParticipantes(List<EntidadeUsuario> lista) throws RemoteException;
 
 }
